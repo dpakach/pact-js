8449b489dce2c2e1d4dc2483e76c9f7f32e4a930
Timothy Jones <timothy.l.jones@gmail.com>
2021-01-04 15:40:49 +1100

    debug(windows): Attempt to do npm link in powershell (I don't know powershell)

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,9 +1,13 @@
 npm run dist
+Push-Location dist
+npm link
+Pop-Location
 
 Get-ChildItem ".\examples" -Directory | ForEach-Object {
   Write-Output "Running examples in $($_.FullName)"
   cd $_.FullName
   npm i
+  npm link @pact-foundation/pact
   npm t
   if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
 }
